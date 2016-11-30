b627ca4fe88dc1174754365512370be45778e649
German Escobar <german.escobarc@gmail.com>
2016-11-30 12:50:03 -0500

    Prepare challenge

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('renders App without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders the checkbox', () => {
+  const wrapper = shallow(<App />);
+  const checkbox = wrapper.find('input[type="checkbox"]');
+  expect(checkbox.length).toBe(1);
+  expect(checkbox.props().checked).toBeFalsy();
+});
+
+it("doesn't renders the paragraph", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('p').length).toBe(0);
+});
+
+it("shows the paragraph when checkbox is checked", () => {
+  const wrapper = shallow(<App />);
+  wrapper.find('input[type="checkbox"]').simulate('click');
+  wrapper.find('input[type="checkbox"]').simulate('change', { target: { checked: true } });
+  expect(wrapper.find('p').length).toBe(1);
 });
