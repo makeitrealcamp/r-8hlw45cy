dbe374e3a78818d52d2ae976ac41d86043596a5d
jeferbc <jeffe.bernal@gmail.com>
2017-10-05 16:46:15 -0500

    Update react, enzyme and configure adapter enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Setup from './Setup';
 
 it('renders App without crashing', () => {
   shallow(<App />);
